a4d9abc719cc25b959a785f5eabb95cc6ac8e645
Alexandr <tolstov.alexandr.85@gmail.com>
2019-06-17 17:47:08 +0600

    Add project files.

---
+++ CustomEnumeratorWithYild/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomEnumeratorWithYild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with the Yield Keyword *****\n");
+            Garage carLOt = new Garage();
+            foreach (Car c in carLOt)
+            {
+                Console.WriteLine("{0} is going {1} MPH", c.PetName, c.CurrentSpeed); 
+            }
+            Console.WriteLine();
+            foreach (Car c in carLOt.GetTheCars(true))
+            {
+                Console.WriteLine("{0} is going {1} MPH", c.PetName, c.CurrentSpeed);
+            }
+            Console.ReadLine();
+        }
+    }
+}
